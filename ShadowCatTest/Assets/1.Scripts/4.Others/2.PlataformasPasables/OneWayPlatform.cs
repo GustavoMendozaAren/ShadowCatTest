c4fbf29884a36b36131af08c5d8f42b6c8ac06ab
GustavoMendozaAren <stavmdza@gmail.com>
2025-04-02 19:16:59 -0600

    LastBuild
    IdiomaTutorial-Plataformas-Balas-Boton

--- ShadowCatTest/Assets/1.Scripts/4.Others/2.PlataformasPasables/OneWayPlatform.cs
+++ ShadowCatTest/Assets/1.Scripts/4.Others/2.PlataformasPasables/OneWayPlatform.cs
@@ -15,7 +15,7 @@ public class OneWayPlatform : MonoBehaviour
     IEnumerator DesactivacionPorSegundos()
     {
         platformCollider.SetActive(false);
-        yield return new WaitForSeconds(1.5f);
+        yield return new WaitForSeconds(0.7f);
         platformCollider.SetActive(true);
     }
 }
